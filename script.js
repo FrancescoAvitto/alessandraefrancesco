baeab710ad72f68bbe8ce10103c1e1acce76fde0
FrancescoAvitto <91961441+FrancescoAvitto@users.noreply.github.com>
2023-10-26 00:45:45 +0200

    aggiunto header, swiper e foto

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
+document.addEventListener("scroll", function () {
+  const navbar = document.querySelector("#navbar");
+  const navbarBoxLogo = document.querySelector("#navbar-box-logo");
+  if (window.scrollY > 200) {
+    navbar.classList.add("bg-white-transparent");
+    navbarBoxLogo.classList.add("hidden");
+  } else {
+    navbar.classList.remove("bg-white-transparent");
+    navbarBoxLogo.classList.remove("hidden");
+  }
+});
+
 // countdown
 
 function timeout() {
   // timer
-  const final = new Date(2024, 9, 14);
+  const final = new Date(2024, 8, 14);
   // const message = document.querySelector("#message");
   const giorniMancanti = document.querySelector("#giorniMancanti");
   const oreMancanti = document.querySelector("#oreMancanti");
@@ -35,16 +47,134 @@ timeout();
 
 // infinite scrolling metodo 1
 
-let photoWrapper = document.querySelector("#photoWrapper");
-console.log(photoWrapper);
+// let photoWrapper = document.querySelector("#photoWrapper");
 
-for (let i = 1; i <= 162; i++) {
-  let photo = document.createElement("div");
-  photo.innerHTML = ` <div class="relative mb-4 m-3">
-  <div class="overflow-hidden">
-    <img class="w-full rounded-md hover:scale-[1.10] duration-[400ms]" src="./media/img/${i}.jpg">
-    </div>
-    </div>`;
+// for (let i = 1; i <= 100; i++) {
+//   let photo = document.createElement("div");
+//   photo.innerHTML = ` <div class="relative mb-4 m-3" >
+//   <div class="overflow-hidden">
+//     <img class="w-full rounded-md hover:scale-[1.10] duration-[400ms]" src="./media/img/${i}.jpg">
+//     </div>
+//     </div>`;
 
-  photoWrapper.appendChild(photo);
+//   photoWrapper.appendChild(photo);
+// }
+
+// infinite scrolling metodo 2
+
+// function generateImageFilesArray(start, end) {
+//   const imageFiles = [];
+
+//   for (let i = start; i <= end; i++) {
+//     const filename = `${i}.jpg`;
+//     imageFiles.push(filename);
+//   }
+
+//   return imageFiles;
+// }
+
+// const imageFiles = generateImageFilesArray(1, 160);
+
+// const card = document.querySelector(".card");
+// const box = document.querySelector(".box");
+// const imageFolder = "/media/img/";
+// let currentImageIndex = 0;
+// const imagesPerBatch = 8;
+
+// function renderImages() {
+//   for (let i = 0; i < imagesPerBatch; i++) {
+//     if (currentImageIndex < imageFiles.length) {
+//       const img = document.createElement("img");
+//       img.src = imageFolder + imageFiles[currentImageIndex];
+//       card.appendChild(img);
+//       currentImageIndex++;
+//       // Verifica se hai mostrato tutte le immagini.
+
+//       if (currentImageIndex >= imageFiles.length) {
+//         console.log(imageFiles.length);
+//         box.classList.add("hidden");
+//         break;
+//       }
+//     }
+//   }
+// }
+
+const loader = document.querySelector(".loader9");
+const card = document.querySelector(".card");
+const imagesPerBatch = 10;
+let currentImageIndex = 0;
+const imagesToDisplay = [];
+const imageFolder = "/media/img/";
+
+function generateImageFilesArray(start, end) {
+  const imageFiles = [];
+
+  for (let i = start; i <= end; i++) {
+    const filename = `${i}.jpg`;
+    imageFiles.push(filename);
+  }
+
+  return imageFiles;
 }
+const imageFiles = generateImageFilesArray(1, 100);
+
+function renderImages() {
+  for (let i = 0; i < imagesPerBatch; i++) {
+    if (currentImageIndex < imageFiles.length) {
+      imagesToDisplay.push(imageFiles[currentImageIndex]);
+      currentImageIndex++;
+
+      if (currentImageIndex >= imageFiles.length) {
+        loader.classList.add("hidden");
+        break;
+      }
+    }
+  }
+
+  card.innerHTML = "";
+
+  imagesToDisplay.forEach((imageFile) => {
+    const img = document.createElement("div");
+    img.innerHTML = `<div class="relative mb-4 m-3" >
+      <div class="overflow-hidden">
+       <img class="w-full rounded-md hover:scale-[1.10] duration-[400ms] overflow-hidden" src="${
+         imageFolder + imageFile
+       }">
+      </div>
+      </div>`;
+    card.appendChild(img);
+  });
+}
+
+window.onscroll = () => {
+  const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
+
+  if (scrollTop + clientHeight + 1 >= scrollHeight) {
+    loader.classList.add("show");
+    renderImages();
+  } else {
+    loader.classList.remove("show");
+  }
+};
+
+// // animate on scroll
+AOS.init({
+  // Global settings:
+  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  startEvent: "DOMContentLoaded", // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: "aos-init", // class applied after initialization
+  animatedClassName: "aos-animate", // class applied on animation
+  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 400, // values from 0 to 3000, with step 50ms
+  easing: "ease", // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: "top-bottom", // defines which position of the element regarding to window should trigger the animation
+});
